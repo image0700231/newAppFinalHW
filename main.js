5fce278910e41392a0c96c8d84ef4e81e60fe1e7
image0700231 <43368490+image0700231@users.noreply.github.com>
2018-12-25 16:56:58 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,151 +5,73 @@
 */
 
 var conversationLogDB = new Mongo.Collection("conversationLog");
+var conversationLog = new ReactiveVar("ELIZA: How are you doing?");
 
-//*********************************************//
-//  請在以下範圍以內修改或新增ELIZA回應的功能函數  //
-//*********************************************//
+Session.setDefault("currentPage", "frontPage");
+Session.setDefault("userSession", "");
 
-var stupidResponse = function(msg) {
-	return "What is "+msg+"?";
-};
-
-var weatherInfo = function(msg) {
-	let wtData;
-	let weatherRegex = /(weather|temperature).* in (\w+)/i;
-	let weatherRequest = msg.match(weatherRegex);
-	if(weatherRequest === null) {
-		return "";
-	}
-	else {
-		let lastPos = weatherRequest.length-1;
-		let cityName = weatherRequest[lastPos];
-		let APIKey = "0f9acd286be670dbec09507843f8f78b";
-		let wtInfoURL = 
-			"http://api.openweathermap.org/data/2.5/weather?APPID="+APIKey+
-			"&q="+cityName+"&units=metric";
-		try {
-			wtData = HTTP.get(wtInfoURL);
-			wtData = wtData.data.main;
-			let wtResponse = "It's "+wtData.temp+"C.";
-			return wtResponse;
-		}
-		catch(error) {
-			return "I don't the city.";
-		}
-		return "";
-	}
-};
-
-/*
-//新增的功能函數皆以以下的程式碼為模板
-var XXXXX = function(msg) {
-	//先進行訊息的檢查，如果訊息檢查符合條件，就執行相關功能並回傳ELIZA的回應
-	if(XXXXX) {
-		return results;	//務必要回傳結果
-	}
-	//不符合條件的話，就回傳一個空字串，讓msgReceiver可以往下呼叫下一個功能函數
-	else {
-		return "";
-	}
-}
-*/
-
-//*********************************************//
-//  請在以上範圍以內修改或新增ELIZA回應的功能函數  //
-//*********************************************//
-
-var initConversation = function(username) {
-	conversationLogDB.insert(
-		{
-			user: username,
-			source: "ELIZA",
-			msg: "Hi, "+username+". How are you doing?",
-			time: new Date()
-		}
-	);
-};
-
-conversationLogDB.deny({
-	insert() {
-		return true;
-	},
-	update() {
-		return true;
-	},
-	remove() {
-		return true;
-	}
+Tracker.autorun(function() {
+  Meteor.subscribe("userConversation", Session.get("userSession"));
 });
 
-Meteor.publish("userConversation", function(username) {
-	return conversationLogDB.find({user: username});
+Template.body.helpers({
+  checkCurrentPage: function(page) {
+    return Session.equals("currentPage", page);
+  }
 });
 
-Meteor.methods({
-	setUser: function(username) {
-		if(username.includes(" ")) {
-			throw new Meteor.Error();
-		}
-		else {
-			let userLog = conversationLogDB.find({user: username}).fetch();
-			if(userLog.length > 0) {
-				return;
-			}
-			else {
-				initConversation(username);
-				return;
-			}
-		}
-	},
-	msgReceiver: function(msg, username) {
-		let dataNum = conversationLogDB.find({user: username}).fetch().length;
-		if(dataNum <= 20) {
-			conversationLogDB.insert(
-				{
-					user: username,
-					source: "You",
-					msg: msg,
-					time: new Date()
-				}
-			);
-			let ELIZAResponse = weatherInfo(msg);
-			//***************************************//
-			//請在以下段落中修改程式碼呼叫不同的功能函數//
-			//獲得ELIZA的回應                        //
-			//**************************************//
+Template.mainSection.helpers({
+  getConversation: function() {
+    let dbData = conversationLogDB.find({}, {sort: {time: 1}});
+    dbData = dbData.fetch();
+    let conversationLog = "";
+    for(let index=0 ; index<dbData.length ; index++) {
+      let msgData = dbData[index];
+      conversationLog = conversationLog+msgData.source+": ";
+      conversationLog = conversationLog+msgData.msg+"\n";
+    }
+    return conversationLog;
+  }
+});
 
-			/*
-			//新增的程式碼開頭一律檢查ELIZAResponse是否還是一個空字串
-			if(ELIZAResponse === "") {
-				//不是空字串的話才呼叫某個功能函數XXXX試圖取得ELIZA的回應
-				ELIZAResponse = XXXXX(msg);
-			}
-			*/
+Template.formSection.events({
+  "click #submitMsg": function(event) {
+    event.preventDefault();
+    let myMsgObj = document.getElementById("myMsg");
+    let myMsg = myMsgObj.value;
+    Meteor.call("msgReceiver", myMsg, Session.get("userSession"), function(error, result) {
+      if(error) {
 
-			if(ELIZAResponse === "") {
-				ELIZAResponse = stupidResponse(msg);
-			}
-			//***************************************//
-			//請在以上段落中修改程式碼呼叫不同的功能函數//
-			//獲得ELIZA的回應                        //
-			//**************************************//
-			conversationLogDB.insert(
-				{
-					user: username,
-					source: "ELIZA",
-					msg: ELIZAResponse,
-					time: new Date()
-				}
-			);
-			return;
-		}
-		else {
-			return "full";
-		}
-	},
-	resetMsg: function(username) {
-		conversationLogDB.remove({user: username});
-		initConversation(username);
-	}
+      }
+      else if(result === "full") {
+        alert("The database is full!");
+      }
+      else {
+
+      }
+    });
+    myMsgObj.value = "";
+  },
+  "click #resetMsg": function() {
+    Meteor.call("resetMsg", Session.get("userSession"));
+  },
+  "click #resetUser": function() {
+    Session.set("userSession", "");
+    Session.set("currentPage", "frontPage");
+  }
+});
+
+Template.frontPage.events({
+  "click #enterMain": function() {
+    let username = document.getElementById("username").value;
+    Meteor.call("setUser", username, function(error, result) {
+      if(error) {
+        alert("Username cannot have any space!");
+      }
+      else {
+        Session.set("userSession", username);
+        Session.set("currentPage", "home");
+      }
+    });
+  }
 });
