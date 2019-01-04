4393a9ca4f11467ee1d3bd0949e861af2143187d
image0700231 <43368490+image0700231@users.noreply.github.com>
2019-01-05 00:44:21 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,73 +5,258 @@
 */
 
 var conversationLogDB = new Mongo.Collection("conversationLog");
-var conversationLog = new ReactiveVar("ELIZA: How are you doing?");
 
-Session.setDefault("currentPage", "frontPage");
-Session.setDefault("userSession", "");
+//*********************************************//
+//  請在以下範圍以內修改或新增ELIZA回應的功能函數  //
 
-Tracker.autorun(function() {
-  Meteor.subscribe("userConversation", Session.get("userSession"));
+// . = any char
+// \w = any letter or number
+// \W = any special symbols
+// \d = any digit
+// \D = any non-digit
+// + = 1 or more than 1 character
+// * = 0 or more than 1 character
+// [] = any character in the scope
+// () = a character group
+
+//*********************************************//
+
+var stupidLog = 
+    [
+       "Tell me more.",
+       "Explain more, please.",
+       "I don't quite understand...",
+       "What does this matter to me?",
+       "Um, what?",
+       "So?",
+       "Okaaaay?",
+       "Whatever, let's talk about something else.",
+       "Really?!",
+       "Cool.",
+       "That's interesting.",
+       "Alright.",
+       "Hmmmmm.",
+       "Wow!",
+       "No comment.",
+       "Why do you want to talk about this?",
+       "Okay. Now, ask me about the weather.",
+       "Anyway, how's your day?",
+       "Is it important?",
+       "Maybe...",    
+    ]
+
+
+
+var stupidResponse = function(msg) {
+	let randomIndex2 = Math.random()*20;
+	randomIndex2 = Math.floor(randomIndex2);
+	return stupidLog[randomIndex2];
+};	
+
+var weatherInfo = function(msg) {
+	let wtData;
+	let weatherRegex = /(weather|temperature).* in (\w+)/i;
+	let weatherRequest = msg.match(weatherRegex);
+	if(weatherRequest === null) {
+		return "";
+	}
+	else {
+		let lastPos = weatherRequest.length-1;
+		let cityName = weatherRequest[lastPos];
+		let APIKey = "0f9acd286be670dbec09507843f8f78b";
+		let wtInfoURL = 
+			"http://api.openweathermap.org/data/2.5/weather?APPID="+APIKey+
+			"&q="+cityName+"&units=metric";
+		try {
+			wtData = HTTP.get(wtInfoURL);
+			wtData = wtData.data.main;
+			let wtResponse = "It's "+wtData.temp+"C.";
+			return wtResponse;
+		}
+		catch(error) {
+			return "I don't know the city.";
+		}
+		return "";
+	}
+};
+
+var randomIndex = Math.random()* 10;
+randomIndex = Math.floor(randomIndex);
+
+let LIResponseLog = 
+		    [
+		       "meditation",
+		       "playing sports such as ice skating",
+		       "reading novels written by Virginia Woolf",
+		       "watching a French movie",
+		       "chatting with me until you are bored",
+		       "drinking 10 cups of coffee",
+		       "taking a nap",
+		       "travelling",
+		       "hanging out with me",
+		       "coding"
+		    ]
+
+var leisureAdvice = function(msg) {
+	let leisureRegex = /.*(fine|great|bored|sad|happy|tired|sick|excited|nervous|angry|heartbroken|anxious|down|blue|lonely|stressed|hopeless|confused|depressed|desperate|melancholy|exhausted|cry|scream|die|bad|soso)|What.*I.*do|.*(suggest|advice|idea|tip)|I.*(thinking|considering)/i;
+	let leisureRequest = msg.match(leisureRegex);
+	if(leisureRequest != null) {
+		LIResponseLog;
+		let LIResponse = "Try "+LIResponseLog[randomIndex]+".";
+		return LIResponse;
+	}
+	else {
+		return "";
+	}
+};
+
+var EPResponseLog = 
+		    [
+		       "The universe works in funny ways, child",
+		       "It's hard to explain",
+		       "Don't ask me because I can't explain, either",
+		       "Because...umm..",
+		       "I have to consult another person, please wait.",
+		       "Life is fickle",
+		       "Google it.",
+		       "You are the first person asking me about this..",
+		       "This is life. Deal with it",
+		       "God knows"
+		    ]
+
+var explanationMaker = function(msg) {
+	let explanationRegex = /why.*|.*(reason|explanation|cause)|.*explain|.*dare/i;
+	let explanationRequest = msg.match(explanationRegex);
+	if(explanationRequest != null) {
+		EPResponseLog;
+		let randomIndex = Math.random()* 10;
+			randomIndex = Math.floor(randomIndex);
+		let EPResponse = EPResponseLog[randomIndex]+".";
+		return EPResponse;
+	}
+	else {
+		return "";
+	}
+};
+
+
+/*
+//新增的功能函數皆以以下的程式碼為模板
+var XXXXX = function(msg) {
+	//先進行訊息的檢查，如果訊息檢查符合條件，就執行相關功能並回傳ELIZA的回應
+	if(XXXXX) {
+		return results;	//務必要回傳結果
+	}
+	//不符合條件的話，就回傳一個空字串，讓msgReceiver可以往下呼叫下一個功能函數
+	else {
+		return "";
+	}
+}
+*/
+
+//*********************************************//
+//  請在以上範圍以內修改或新增ELIZA回應的功能函數  //
+//*********************************************//
+
+var initConversation = function(username) {
+	conversationLogDB.insert(
+		{
+			user: username,
+			source: "ELIZA",
+			msg: "Hi, "+username+". How are you doing?",
+			time: new Date()
+		}
+	);
+};
+
+conversationLogDB.deny({
+	insert() {
+		return true;
+	},
+	update() {
+		return true;
+	},
+	remove() {
+		return true;
+	}
 });
 
-Template.body.helpers({
-  checkCurrentPage: function(page) {
-    return Session.equals("currentPage", page);
-  }
+Meteor.publish("userConversation", function(username) {
+	return conversationLogDB.find({user: username});
 });
 
-Template.mainSection.helpers({
-  getConversation: function() {
-    let dbData = conversationLogDB.find({}, {sort: {time: 1}});
-    dbData = dbData.fetch();
-    let conversationLog = "";
-    for(let index=0 ; index<dbData.length ; index++) {
-      let msgData = dbData[index];
-      conversationLog = conversationLog+msgData.source+": ";
-      conversationLog = conversationLog+msgData.msg+"\n";
-    }
-    return conversationLog;
-  }
-});
+Meteor.methods({
+	setUser: function(username) {
+		if(username.includes(" ")) {
+			throw new Meteor.Error();
+		}
+		else {
+			let userLog = conversationLogDB.find({user: username}).fetch();
+			if(userLog.length > 0) {
+				return;
+			}
+			else {
+				initConversation(username);
+				return;
+			}
+		}
+	},
+	msgReceiver: function(msg, username) {
+		let dataNum = conversationLogDB.find({user: username}).fetch().length;
+		if(dataNum <= 20) {
+			conversationLogDB.insert(
+				{
+					user: username,
+					source: "You",
+					msg: msg,
+					time: new Date()
+				}
+			);
+			let ELIZAResponse = weatherInfo(msg);
 
-Template.formSection.events({
-  "click #submitMsg": function(event) {
-    event.preventDefault();
-    let myMsgObj = document.getElementById("myMsg");
-    let myMsg = myMsgObj.value;
-    Meteor.call("msgReceiver", myMsg, Session.get("userSession"), function(error, result) {
-      if(error) {
+			if(ELIZAResponse === "") {
+				ELIZAResponse = leisureAdvice(msg);
+			}
+			if(ELIZAResponse === "") {
+				ELIZAResponse = explanationMaker(msg);
+			}
+			
+			//***************************************//
+			//請在以下段落中修改程式碼呼叫不同的功能函數//
+			//獲得ELIZA的回應                        //
+			//**************************************//
 
-      }
-      else if(result === "full") {
-        alert("The database is full!");
-      }
-      else {
+			/*
+			//新增的程式碼開頭一律檢查ELIZAResponse是否還是一個空字串
+			if(ELIZAResponse === "") {
+				//不是空字串的話才呼叫某個功能函數XXXX試圖取得ELIZA的回應
+				ELIZAResponse = XXXXX(msg);
+			}
+			*/
 
-      }
-    });
-    myMsgObj.value = "";
-  },
-  "click #resetMsg": function() {
-    Meteor.call("resetMsg", Session.get("userSession"));
-  },
-  "click #resetUser": function() {
-    Session.set("userSession", "");
-    Session.set("currentPage", "frontPage");
-  }
-});
-
-Template.frontPage.events({
-  "click #enterMain": function() {
-    let username = document.getElementById("username").value;
-    Meteor.call("setUser", username, function(error, result) {
-      if(error) {
-        alert("Username cannot have any space!");
-      }
-      else {
-        Session.set("userSession", username);
-        Session.set("currentPage", "home");
-      }
-    });
-  }
+			if(ELIZAResponse === "") {
+				ELIZAResponse = stupidResponse(msg);
+			}
+			//***************************************//
+			//請在以上段落中修改程式碼呼叫不同的功能函數//
+			//獲得ELIZA的回應                        //
+			//**************************************//
+			conversationLogDB.insert(
+				{
+					user: username,
+					source: "ELIZA",
+					msg: ELIZAResponse,
+					time: new Date()
+				}
+			);
+			return;
+		}
+		else {
+			return "full";
+		}
+	},
+	resetMsg: function(username) {
+		conversationLogDB.remove({user: username});
+		initConversation(username);
+	}
 });
